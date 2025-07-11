ad84c2c2a4d6212ee910a7b9989d2b89b0721aeb
D. Sohan Kumar <sohans1092004@gmail.com>
2025-07-11 16:01:42 +0530

    feat: add models for comments, likes, playlists, and tweets

---
+++ src/models/like.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    video:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video",
+    },
+    comment:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment",
+    },
+    tweet:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tweet",
+    },
+    likedBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }
+},{timestamps:true})
+
+export const like = mongoose.model("Like",likeSchema)
---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        trim:true,
+    },
+    coverImage:{
+        type:String,//cloudinary url
+    },
+    videos:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    }],
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }
+},{timestamps:true})
+
+export const playlist = mongoose.model("Playlist",playlistSchema)
